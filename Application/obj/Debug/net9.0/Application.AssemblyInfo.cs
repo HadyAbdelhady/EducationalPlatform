5ea198b8fc841eac701d0e3f31929ee7c8c74564
Usef85 <youssefroaf94@gmail.com>
2025-11-16 20:46:24 +0200

    Assemblies

--- Application/obj/Debug/net9.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net9.0/Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+285f73c24e40be98d5cb652c2737a9c8cd189904")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1adfc1a3335d1d3b3e84085fdc4deafd1d57ed92")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
