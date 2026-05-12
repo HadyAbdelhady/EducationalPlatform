27315b13d05293936d407ba89a6b30203de950c7
HadyAbdelhady <hady011200@gmail.com>
2026-05-12 19:54:18 +0300

    modification in the video progress (get by ID)

--- Application/Features/Videos/Queries/GetVideoById/GetVideoByIdQueryHandler.cs
+++ Application/Features/Videos/Queries/GetVideoById/GetVideoByIdQueryHandler.cs
@@ -15,7 +15,9 @@ namespace Application.Features.Videos.Queries.GetVideoById
         {
             try
             {
-                var video = await _unitOfWork.Repository<Video>().GetByIdAsync(request.VideoId, cancellationToken);
+                var video = _unitOfWork.Repository<Video>().Find(v => v.Id == request.VideoId && v.StudentVideos.Any(sv => sv.StudentId == request.StudentId),
+                                                                cancellationToken,
+                                                                sv => sv.StudentVideos).FirstOrDefault();
 
                 if (video is null)
                     return Result<VideoByUserIdResponse>.FailureStatusCode("Video not found.", ErrorType.NotFound);
@@ -26,9 +28,9 @@ namespace Application.Features.Videos.Queries.GetVideoById
                     Name = video.Name,
                     VideoUrl = video.VideoUrl,
                     Description = video.Description,
-                    Progress = video.StudentVideos.Where(s => s.StudentId == request.StudentId && s.VideoId == video.Id).Select(s => s.Progress).FirstOrDefault(),
-                    NumberOfQuestionsSheets = video.Sheets.Where(sh => sh.Type == SheetType.QuestionSheet).Count(),
-                    NumberOfTutorialSheets = video.Sheets.Where(sh => sh.Type == SheetType.TutorialSheet).Count(),
+                    Progress = video.StudentVideos.Select(s => s.Progress).FirstOrDefault(),
+                    NumberOfQuestionsSheets = video.Sheets.Count(sh => sh.Type == SheetType.QuestionSheet),
+                    NumberOfTutorialSheets = video.Sheets.Count(sh => sh.Type == SheetType.TutorialSheet),
                     SectionId = video.SectionId,
                     CreatedAt = video.CreatedAt,
                     UpdatedAt = video.UpdatedAt ?? video.CreatedAt,
